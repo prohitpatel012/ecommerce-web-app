2024b2d749702b52c5adb33e59f6f0161775213d
rohitpatel-dev <rohitrampatel185@gmail.com>
2025-12-04 08:18:23 +0530

    Header template ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello World</h1>
+    <div className="">
+
+      <Header />
+      <h1>Application Body data</h1>
+
+
     </div>
   );
 }
